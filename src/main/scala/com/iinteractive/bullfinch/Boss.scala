017b88b167f9d094e08a15673659d8c1a7e5e44e
Cory G Watson <gphat@onemogin.com>
2012-06-18 14:03:08 -0500

    More config work.

--- src/main/scala/com/iinteractive/bullfinch/Boss.scala
+++ src/main/scala/com/iinteractive/bullfinch/Boss.scala
@@ -1,11 +1,12 @@
 package com.iinteractive.bullfinch
 
+import com.iinteractive.bullfinch.util.ConfigReader
 import grizzled.slf4j.Logging
 import java.net.URL
 
 class Boss(configs : Seq[URL]) extends Logging {
   
-  val configURLs = configs
+  val configURLs = ConfigReader.read(configs)
   val configTimestamps : Seq[Long] = Seq()
   
   def restart() {
---
+++ src/test/scala/com/iinteractive/bullfinch/BossSpec.scala
@@ -0,0 +1,24 @@
+package com.iinteractive.bullfinch
+
+import org.specs2.mutable._
+
+import com.iinteractive.Bullfinch
+import com.iinteractive.bullfinch.util.ConfigReader
+
+class BossSpec extends Specification {
+
+  "The Boss" should {
+    "fail if there are no config urls" in {
+      Bullfinch.main(Seq().toArray) must throwA[RuntimeException]
+    }
+
+    "fail if there are all bad config urls" in {
+      Bullfinch.main(Seq("crap").toArray) must throwA[RuntimeException]
+    }
+    
+    "succeed with real urls" in {
+      Bullfinch.main(Seq("http://www.example.com").toArray)
+      1 mustEqual 1 // Junk, we'll throw an exception if the above fails
+    }
+  }
+}
