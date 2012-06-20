7c2b4057e1b575ee285f5f95610c1af86c8fb51f
Cory G Watson <gphat@onemogin.com>
2012-06-20 14:26:42 -0500

    Hint of strings.

--- src/main/scala/com/iinteractive/bullfinch/util/JSONResultSetWrapper.scala
+++ src/main/scala/com/iinteractive/bullfinch/util/JSONResultSetWrapper.scala
@@ -5,6 +5,7 @@ import java.sql.{ResultSet,Types}
 import com.codahale.jerkson.Json._
 
 case class Column(
+  index: Int,
   label: String,
   ctype: Int
 )
@@ -21,6 +22,7 @@ class JSONResultSetWrapper(resultSet: ResultSet) extends Iterator[String] with L
     // +1 since we're doing things 1 based, not 0
     1 to (md.getColumnCount) map { i =>
       Column(
+        index = i,
         label = md.getColumnLabel(i),
         ctype = md.getColumnType(i)
       )
@@ -54,6 +56,13 @@ class JSONResultSetWrapper(resultSet: ResultSet) extends Iterator[String] with L
     var obj = scala.collection.mutable.Map[String,Any]()
     
     obj += ("row_num" -> resultSet.getRow)
+    
+    columns.foreach { col =>
+      col.ctype match {
+        case Types.CHAR | Types.VARCHAR | Types.LONGVARCHAR => obj += (col.label -> resultSet.getString(col.index))
+        case _ => // Nothing!
+      }
+    }
     
     // Reset the sentinel so that the next hasNext will work
     checkedNext = false
