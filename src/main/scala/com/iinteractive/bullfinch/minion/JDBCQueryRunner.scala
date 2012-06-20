c0a3198a1ba54a78b428452a332d70697ad026c0
Cory G Watson <gphat@onemogin.com>
2012-06-20 08:55:13 -0500

    Config code for jdbc query runner

--- src/main/scala/com/iinteractive/bullfinch/minion/JDBCQueryRunner.scala
+++ src/main/scala/com/iinteractive/bullfinch/minion/JDBCQueryRunner.scala
@@ -4,6 +4,8 @@ import com.codahale.jerkson.AST._
 import com.codahale.jerkson.Json._
 import com.codahale.jerkson.JsonSnakeCase
 import com.iinteractive.bullfinch._
+import java.util.{ArrayList,LinkedHashMap}
+import scala.collection.JavaConversions._
 
 @JsonSnakeCase
 case class Request(
@@ -13,6 +15,24 @@ case class Request(
 )
 
 class JDBCQueryRunner(config: Option[Map[String,Any]]) extends Minion(config) with QueueMonitor with JDBCBased {
+
+  val statementConfig = config.get("statements").asInstanceOf[LinkedHashMap[String,Object]].toMap
+  val statements = statementConfig.mapValues { more =>
+    val newmap = more.asInstanceOf[LinkedHashMap[String,Object]].toMap
+    val sql = newmap.get("sql") match {
+      case Some(sql) => sql.asInstanceOf[String]
+      case None => throw new RuntimeException("JDBC Query Runner worker statements must have SQL")
+    }
+    val params = newmap.get("params") match {
+      case Some(p) => { asScalaBuffer(p.asInstanceOf[ArrayList[String]]) }
+      case None => None
+    }
+    Map(
+      "sql"   -> sql,
+      "params"-> params
+    )
+  }
+  log.error("STATEMENTS: " + statements)
 
   override def configure {
     super.configure
