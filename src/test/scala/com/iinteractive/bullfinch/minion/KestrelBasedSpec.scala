8440c8af71222d6d216af0d3950ce14636ec9459
Cory G Watson <gphat@onemogin.com>
2012-06-27 12:16:47 -0500

    Further kestrel tests.

--- src/test/scala/com/iinteractive/bullfinch/minion/KestrelBasedSpec.scala
+++ src/test/scala/com/iinteractive/bullfinch/minion/KestrelBasedSpec.scala
@@ -2,6 +2,7 @@ package com.iinteractive.bullfinch.minion
 
 import org.specs2.mutable._
 import com.iinteractive.bullfinch.Minion
+import java.util.concurrent.TimeUnit
 
 import org.specs2.mock._
 import net.spy.memcached.MemcachedClient
@@ -19,10 +20,24 @@ class KestrelBasedSpec extends Specification with Mockito {
       val mockClient: MemcachedClient = mock[MemcachedClient]
 
       val kes = new TestKestrelMinion(config = None)
-      kes.protoClient = Some(mockClient)
-
-      kes.sendMessage(queue = "someQueue", message = "Message")
-      there was one(mockClient).set("someQueue", 0, "Message")
+      1 must beEqualTo(1)
     }
+  }
+  
+  "properly calls Memcached client" in {
+    val mockClient: MemcachedClient = mock[MemcachedClient]
+
+    val kes = new TestKestrelMinion(config = None)
+    kes.protoClient = Some(mockClient)
+
+    kes.sendMessage(queue = "someQueue", message = "Message")
+    there was one(mockClient).set("someQueue", 0, "Message")
+    
+    kes.getMessage(queue = "someQueue", tout = 1234)
+    kes.confirm(queue = "someQueue")
+    there was one(mockClient).get("someQueue/open/t=1234") then one(mockClient).get("someQueue/close")
+    
+    kes.cancel
+    there was one(mockClient).shutdown(1, TimeUnit.SECONDS)
   }
 }
